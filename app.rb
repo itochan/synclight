a1c2ab68c58b9db3068fad27e48469c1d14bdd5e
Kazunori Jo <itochan315@gmail.com>
2017-07-08 19:32:15 +0900

    Use require 'sinatra'

--- app.rb
+++ app.rb
@@ -1,48 +1,44 @@
 require 'json'
 
-require 'sinatra/base'
+require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra-websocket'
 
 require 'slim'
 
-class App < Sinatra::Base
-  set :sockets, []
-  set :color, '#ffffff'
+set :sockets, []
+set :color, '#ffffff'
 
-  get '/' do
-    slim :index
-  end
+get '/' do
+  slim :index
+end
 
-  get '/websocket' do
-    if request.websocket?
-      request.websocket do |ws|
-        ws.onopen do
-          settings.sockets << ws
-          ws.send(settings.color)
-        end
-        ws.onclose do
-          settings.sockets.delete(ws)
-        end
+get '/websocket' do
+  if request.websocket?
+    request.websocket do |ws|
+      ws.onopen do
+        settings.sockets << ws
+        ws.send(settings.color)
+      end
+      ws.onclose do
+        settings.sockets.delete(ws)
       end
     end
   end
+end
 
-  get '/console' do
-    slim :console
+get '/console' do
+  slim :console
+end
+
+post '/color/update' do
+  params = JSON.parse(request.body.read)
+
+  settings.color = params['color']
+  settings.sockets.each do |s|
+    s.send(settings.color)
   end
 
-  post '/color/update' do
-    params = JSON.parse(request.body.read)
-
-    settings.color = params['color']
-    settings.sockets.each do |s|
-      s.send(settings.color)
-    end
-
-    content_type :json
-    { color: settings.color }.to_json
-  end
-
-  run! if app_file == $0
+  content_type :json
+  { color: settings.color }.to_json
 end
