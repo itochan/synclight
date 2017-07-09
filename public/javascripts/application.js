594bceee0463090c8cb728b3a80a19242f47e495
Kazunori Jo <itochan315@gmail.com>
2017-07-09 14:35:29 +0900

    Use wss if using HTTPS

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,15 @@
 (function (){
   window.onload = function(){
-    var ws = new WebSocket('ws://' + window.location.host + "/websocket");
+    var wsProtocol = window.location.protocol == 'https:' ? 'wss' : 'ws';
+    var ws = new WebSocket(wsProtocol + '://' + window.location.host + '/websocket');
 
     var light = document.getElementById('light');
 
     ws.onopen = function() {
-      console.log("connection opened");
+      console.log('connection opened');
     }
     ws.onclose = function() {
-      console.log("connection closed");
+      console.log('connection closed');
     }
     ws.onmessage = function(m) {
       light.style.backgroundColor = m.data;
