e43fa23ff3e6049e129fc88869b388543f794d53
Kazunori Jo <itochan315@gmail.com>
2017-07-09 14:32:00 +0900

    Use Sinatra::Base

--- app.rb
+++ app.rb
@@ -9,59 +9,61 @@ require 'slim'
 set :sockets, []
 set :color, '#ffffff'
 
-get '/' do
-  slim :index
-end
+class App < Sinatra::Base
+  get '/' do
+    slim :index
+  end
 
-get '/websocket' do
-  if request.websocket?
-    request.websocket do |ws|
-      ws.onopen do
-        settings.sockets << ws
-        ws.send(settings.color)
-      end
-      ws.onclose do
-        settings.sockets.delete(ws)
+  get '/websocket' do
+    if request.websocket?
+      request.websocket do |ws|
+        ws.onopen do
+          settings.sockets << ws
+          ws.send(settings.color)
+        end
+        ws.onclose do
+          settings.sockets.delete(ws)
+        end
       end
     end
   end
-end
 
-get '/console' do
-  slim :console
-end
-
-put '/config/upload' do
-  content_type :json
-
-  unless params[:file] || params[:name]
-    return { status: "fail!" }.to_json
+  get '/console' do
+    slim :console
   end
 
-  filename = "#{params[:name]}.json"
-  save_path = "./public/config/#{filename}"
-  File.open(save_path, 'wb') do |f|
-		p params[:file][:tempfile]
-		f.write params[:file][:tempfile].read
-  end
-  return { config: filename, status: "success" }.to_json
-end
+  put '/config/upload' do
+    content_type :json
 
-get '/config/load/:name' do
-  content_type :json
+    unless params[:file] || params[:name]
+      return { status: "fail!" }.to_json
+    end
 
-  save_path = "./public/config/#{params[:name]}"
-  return File.read(save_path)
-end
-
-post '/color/update' do
-  params = JSON.parse(request.body.read)
-
-  settings.color = params['color']
-  settings.sockets.each do |s|
-    s.send(settings.color)
+    filename = "#{params[:name]}.json"
+    save_path = "./public/config/#{filename}"
+    File.open(save_path, 'wb') do |f|
+      p params[:file][:tempfile]
+      f.write params[:file][:tempfile].read
+    end
+    return { config: filename, status: "success" }.to_json
   end
 
-  content_type :json
-  { color: settings.color }.to_json
+  get '/config/load/:name' do
+    content_type :json
+
+    save_path = "./public/config/#{params[:name]}"
+    return File.read(save_path)
+  end
+
+  post '/color/update' do
+    params = JSON.parse(request.body.read)
+
+    settings.color = params['color']
+    settings.sockets.each do |s|
+      s.send(settings.color)
+    end
+
+    content_type :json
+    { color: settings.color }.to_json
+  end
 end
